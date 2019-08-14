c7d47c75bfad648d22a61dab44e6b1fd4e78f47e
knilecrack <mcmausknile@hotmail.com>
2019-08-14 06:47:44 +0200

    Added firewall

--- GetEVents.ps1
+++ GetEVents.ps1
@@ -10,3 +10,13 @@ foreach ($log in $securityLogs) {
     $arrayOfIps += $ip
   }
 }
+
+
+New-NetFirewallRule -DisplayName "BlockRule2" -Direction Inbound -LocalPort Any -Protocol any -Action Block -RemoteAddress 185.156.177.42
+$gc = Get-NetFirewallRule -Name "{14b145ab-a08a-4b09-a9ea-d9752cd49658}" | Get-NetFirewallAddressFilter
+$gc.RemoteAddress
+#TODO: add logs
+#function to add added IP addresses to txt.log
+
+#TODO:
+#connect to mikrotik and add IP address as blacklist
